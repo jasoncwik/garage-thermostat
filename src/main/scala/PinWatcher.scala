49fcc55e5ffae7cd014769a1c9321ea7e8575308
Jason Cwik <Jason.Cwik@emc.com>
2017-10-01 19:33:22 -0500

    Poll pins explicitly once a minute to deal with random glitches.

--- src/main/scala/PinWatcher.scala
+++ src/main/scala/PinWatcher.scala
@@ -1,15 +1,17 @@
-import PinWatcher.{PinNotify, Unwatch, Watch}
-import akka.actor.Actor.Receive
+import PinWatcher.{Poll, Unwatch, Watch}
 import akka.actor.{Actor, ActorRef, Props}
 import akka.event.Logging
 import com.pi4j.io.gpio.event.{GpioPinDigitalStateChangeEvent, GpioPinListenerDigital}
 import com.pi4j.io.gpio.{GpioController, GpioPinDigitalInput, Pin, PinPullResistance}
+import scala.concurrent.duration._
+
 
 object PinWatcher {
   def props(controller:GpioController, pin:Pin, notify:ActorRef) = Props(new PinWatcher(controller, pin, notify))
 
   case object Watch
   case object Unwatch
+  case object Poll
   case class PinNotify(state:Boolean, from:ActorRef)
 }
 
@@ -21,9 +23,13 @@ class PinWatcher(controller:GpioController, pin:Pin, notify:ActorRef) extends Ac
 
   protected val input:GpioPinDigitalInput = controller.provisionDigitalInputPin(pin, PinPullResistance.PULL_DOWN)
 
+  // Once a minute, poll the actual pin state.  I've seen some glitches and this should reset them.
+  this.context.system.scheduler.schedule(1 minute, 1 minute, self, Poll)
+
   override def receive: Receive = {
     case Watch => input.addListener(this); notify ! PinWatcher.PinNotify(input.getState.isHigh, self)
     case Unwatch => input.removeListener(this)
+    case Poll => notify ! PinWatcher.PinNotify(input.isHigh, self)
     case _ => log.info("received unknown message")
   }
 
